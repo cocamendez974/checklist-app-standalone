e1b10c998a82145838f32003737dfc08fb6147a4
cocamendez974 <cocamendez974@gmail.com>
2025-06-01 18:40:19 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'checklist-app-standalone',
+  webDir: 'www'
+};
+
+export default config;
